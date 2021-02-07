9602677479ce9605a2ac5e555101929fa506d9b4
Abhi <adevarap@cmu.edu>
2021-02-07 17:31:35 -0500

    finished chores and tasks models

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,21 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :pending, -> {where(completed: false)}
+    scope :done, -> {where(completed: true)}
+    scope :upcoming, -> {where('due_on >=?', Date.today)}
+    scope :past, -> {where('due_on <?', Date.today)}
+    scope :by_task, -> { order('task.name') }
+    scope :chronological, -> { order('due_on') }
+
+    def status
+        if self.completed == true
+            return "Completed"
+        else
+            return "Pending"
+        end
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+    has_many :chores
+    has_many :children, through: :chores
+
+    validates_presence_of :name
+    validates_numericality_of :points
+    validates :points, numericality: { only_integer: true }
+    validates :points, numericality: { greater_than: 0 }
+    scope :active, -> {where(active: true)}
+    scope :alphabetical, -> {order('name')}
 end
