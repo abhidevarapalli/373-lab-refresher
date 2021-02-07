02e482bb2672e124dbe274357c263befa707ee76
Abhi <adevarap@cmu.edu>
2021-02-07 16:54:22 -0500

    same

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -3,14 +3,11 @@ require 'test_helper'
 class TaskTest < ActiveSupport::TestCase
   should have_many(:chores)
   should have_many(:children).through(:chores)
-
   should validate_presence_of(:name)
   should validate_numericality_of(:points)
-  
   should allow_value(1).for(:points)
   should allow_value(10).for(:points)
   should allow_value(100).for(:points)
-  
   should_not allow_value("bad").for(:points)
   should_not allow_value(-2).for(:points)
   should_not allow_value(3.14159).for(:points)
@@ -32,5 +29,4 @@ class TaskTest < ActiveSupport::TestCase
       assert_equal ["Mow grass", "Shovel driveway", "Sweep floor", "Wash dishes"], Task.active.alphabetical.map{|t| t.name}
     end
   end
-
 end
